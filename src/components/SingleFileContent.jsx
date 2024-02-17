a6c24f8b81ba836e858c24a96e828a4a64fbff8d
Tharana Wanigaratne <xenocodecs@gmail.com>
2024-02-17 15:42:07 +0530

    Update add offset index to start files from line 1 instead of 0

--- src/components/SingleFileContent.jsx
+++ src/components/SingleFileContent.jsx
@@ -2,13 +2,14 @@ import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 
 
+const REACT_REPORT_OFFSET = 1;
 function SingleFileContent({ issues, fileContent }) {
     const [issueLines, setIssueLines] = useState([])
 
     useEffect(() => {
         if (issues?.length !== 0) {
             issues?.forEach((issue) => {
-                const newIssueLine = issue.textRange.startLine;
+                const newIssueLine = issue.textRange.startLine + REACT_REPORT_OFFSET;
 
                 setIssueLines(prevIssueLines => [...prevIssueLines, newIssueLine]);
             })
@@ -21,6 +22,7 @@ function SingleFileContent({ issues, fileContent }) {
 }
 
 const ContentMapper = ({ fileContent, issueLines }) => {
+    // Create a column of line numbers and the content of the file
     return (
         <Box sx={{
             display: "flex",
@@ -36,6 +38,7 @@ const ContentMapper = ({ fileContent, issueLines }) => {
             border: "1px solid var(--primary-color)",
             overflowX: "auto",
         }}>
+
             {
                 fileContent.split("\n").map((line, index) => {
                     return (
@@ -43,11 +46,11 @@ const ContentMapper = ({ fileContent, issueLines }) => {
                             margin: "0",
                             padding: "0.3rem 1rem",
                             // backgroundColor: "#f5f5f5",
-                            backgroundColor: issueLines.includes(index) ? "#F74B5A" : "#f5f5f5",
+                            backgroundColor: issueLines.includes(index + REACT_REPORT_OFFSET) ? "#F74B5A" : "#f5f5f5",
                         }}>
-                            <Box key={index} sx={{ display: "flex", gap: "1rem" }}>
+                            <Box key={index + REACT_REPORT_OFFSET} sx={{ display: "flex", gap: "1rem" }}>
                                 <Box>
-                                    {index}
+                                    {index + REACT_REPORT_OFFSET}
                                 </Box>
                                 <Box>
                                     {line}
--- src/components/SingleFileTable.jsx
+++ src/components/SingleFileTable.jsx
@@ -92,6 +92,7 @@ const columns = [
 
 function SingleFileTable({ issues }) {
     const [rows, setRows] = useState([])
+    const REACT_REPORT_OFFSET = 1;
 
     useEffect(() => {
         if (issues?.length !== 0) {
@@ -100,7 +101,7 @@ function SingleFileTable({ issues }) {
                     id: issueID,
                     ruleID: issue.ruleID,
                     issueType: issue.type,
-                    lineRange: `(${issue.textRange.startLine}:${issue.textRange.startLineOffset}, ${issue.textRange.endLine}:${issue.textRange.endLineOffset})`,
+                    lineRange: `(${issue.textRange.startLine + REACT_REPORT_OFFSET}:${issue.textRange.startLineOffset}, ${issue.textRange.endLine}:${issue.textRange.endLineOffset + REACT_REPORT_OFFSET})`,
                     description: issue.message
                 }
 
